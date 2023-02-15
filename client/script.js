07ab6d71ad99473e1ad15051660fbb98d9feb308
gadgetandgearings <afifbinalam2007@gmail.com>
2023-02-15 18:10:01 +0600

    update the server url

--- client/script.js
+++ client/script.js
@@ -86,7 +86,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch('http://localhost:5000', {
+    const response = await fetch('https://codex-dq6o.onrender.com/', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
@@ -114,7 +114,7 @@ const handleSubmit = async (e) => {
 
 form.addEventListener('submit', handleSubmit)
 form.addEventListener('keyup', (e) => {
-    if (e.keyCode === 13) {
+if (e.keyCode === 13) {
         handleSubmit(e)
     }
 })
